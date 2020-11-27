b79350231223c009dc170aa60c315300434bbc85
trevcyb <trevontewari@gmail.com>
2020-11-26 22:32:13 -0400

    Everything works, code is messy

--- library.js
+++ library.js
@@ -3,12 +3,11 @@ let bookName;
 let bookAuthor;
 let bookPage;
 let bookIsRead;
-let i = 10;
+let i = 9;
 const addnewBook = document.querySelector('#newBook');
 const closeFormBtn = document.querySelector('#formCancel');
 const submitBook = document.querySelector('#formSubmit');
 const bookDisplay = document.querySelector('#bookDisplay');
-const bookdivs = document.querySelectorAll('.book-item');
 
 myLibrary[0] = new Book("The Witcher", "Andrzej Sapkowski", 500, false);
 myLibrary[1] = new Book("Harry Potter", "J.K. Rowling", 600, true);
@@ -27,8 +26,8 @@ function Book(name, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-    this.info = function() {
-        return(name + " by " + author + ", " + pages + " pages, " + isRead);
+    this.info = function () {
+        return (name + " by " + author + ", " + pages + " pages, " + isRead);
     }
 }
 
@@ -54,9 +53,9 @@ function addBooktoArray() {
 function displayBook() {
     for (c = 0; c < myLibrary.length; c++) {
         let cell = document.createElement('div');
-        cell.setAttribute("tabindex","0");
-        cell.setAttribute("dataid", c)
-        cell.innerHTML = '<h2>' + myLibrary[c].name + '</h2>' + '<h3>' + myLibrary[c].author + '</h3>' ;
+        cell.setAttribute("tabindex", "0");
+        cell.setAttribute("data-bookid", c)
+        cell.innerHTML = '<h2>' + myLibrary[c].name + '</h2>' + '<h3>' + myLibrary[c].author + '</h3>';
         bookDisplay.appendChild(cell).className = "book-item";
 
         let btnset = document.createElement('div');
@@ -67,14 +66,67 @@ function displayBook() {
         let readBtn = document.createElement('button');
         remBtn.id = "remBtn";
         remBtn.setAttribute = ("type", "button");
-        remBtn.innerHTML = "&#10008";
-        readBtn.id ="readBtn";
+        remBtn.innerHTML = "&#10008 Remove Book";
+        readBtn.id = "readBtn";
         readBtn.setAttribute = ("type", "button");
-        readBtn.innerHTML = "&#10004";
+
+        if (myLibrary[c].isRead === true) {
+            readBtn.innerHTML = "&#128513 Book Read";
+        } else if (myLibrary[c].isRead === false) {
+            readBtn.innerHTML = "&#128517 Haven't Started";
+        }
+        
         btnset = document.querySelectorAll(".btnset");
-        btnset.forEach(btn => btn.appendChild(remBtn).className = "editbtn");
-        btnset.forEach(btn => btn.appendChild(readBtn).className = "editbtn");
+        btnset.forEach(btn => btn.appendChild(remBtn).className = "editbtn" + c);
+        btnset.forEach(btn => btn.appendChild(readBtn).className = "editbtn" + c);
+
     }
+
+    let bookitems = document.querySelectorAll(".book-item");
+
+    bookitems.forEach(bookdiv => bookdiv.addEventListener("mouseover", function () {
+        let chosen = document.querySelectorAll(".editbtn" + this.dataset.bookid);
+        for (let chs of chosen) {
+            chs.style.display = "inline-block";
+        }
+    }));
+
+    bookitems.forEach(bookdiv => bookdiv.addEventListener("mouseout", function () {
+        let chosen = document.querySelectorAll(".editbtn" + this.dataset.bookid);
+        for (let chs of chosen) {
+            chs.style.display = "none";
+        }
+    }));
+
+    const readStatus = document.querySelectorAll('#readBtn');
+    readStatus.forEach(bk => bk.addEventListener("click", changeReadStatus));
+
+    const delBook = document.querySelectorAll('#remBtn');
+    delBook.forEach(bk => bk.addEventListener("click", removeBook));
+
+}
+
+function changeReadStatus(event) {
+    if (myLibrary[event.target.parentElement.parentElement.dataset.bookid].isRead === true) {
+        myLibrary[event.target.parentElement.parentElement.dataset.bookid].isRead = false
+    } else if (myLibrary[event.target.parentElement.parentElement.dataset.bookid].isRead === false) {
+        myLibrary[event.target.parentElement.parentElement.dataset.bookid].isRead = true
+    }
+    console.log(myLibrary[event.target.parentElement.parentElement.dataset.bookid].isRead)
+    let bookGrid = document.querySelectorAll(".book-item");
+    bookGrid.forEach(book => book.remove());
+
+    displayBook();
+}
+
+function removeBook(event) {
+    myLibrary.splice(event.target.parentElement.parentElement.dataset.bookid, 1);
+    alert("Item was removed");
+
+    let bookGrid = document.querySelectorAll(".book-item");
+    bookGrid.forEach(book => book.remove());
+
+    displayBook();
 }
 
 function openForm() {
@@ -85,15 +137,9 @@ function closeForm() {
     document.getElementById("popupForm").style.display = "none";
 }
 
-function showBtns() {
-    document.getElementById("readBtn").style.display = "inline-block";
-    document.getElementById("remBtn").style.display = "inline-block";
-}
 
 addnewBook.addEventListener("click", openForm);
 
 submitBook.addEventListener("click", addBooktoArray);
 
 closeFormBtn.addEventListener("click", closeForm);
-
-bookdivs.forEach(bookdiv => bookdiv.addEventListener("click", showBtns));
